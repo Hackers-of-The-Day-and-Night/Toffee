b63288f2167ce20225e64d6159583922c519c96d
Ahmed Shaaban <ahmedlearningscience@gmail.com>
2023-05-13 16:33:07 +0300

    Update GeneralMethods - Add GetIntInput() - Add GetCharInput() - Add GetDoubleInput() - Add GetBooleanInput()

--- src/gm/GeneralMethods.java
+++ src/gm/GeneralMethods.java
@@ -8,4 +8,34 @@ public class GeneralMethods {
     System.out.println(prompt);
     return in.nextLine();
   }
+
+  public static int GetIntInput(String prompt) {
+    Scanner in = new Scanner(System.in);
+    System.out.println(prompt);
+    return in.nextInt();
+  }
+
+  public static double GetDoubleInput(String prompt) {
+    Scanner in = new Scanner(System.in);
+    System.out.println(prompt);
+    return in.nextDouble();
+  }
+
+  public static boolean GetBooleanInput(String prompt) {
+    Scanner in = new Scanner(System.in);
+    char c;
+    do {
+      System.out.println(prompt + " (y/n)");
+      c = Character.toLowerCase(in.nextLine().charAt(0));
+    } while (c != 'y' && c != 'n');
+    return c == 'y';
+  }
+
+  public static char GetCharInput(String prompt) {
+    Scanner in = new Scanner(System.in);
+    char c;
+    System.out.println(prompt);
+    c = Character.toLowerCase(in.nextLine().charAt(0));
+    return c;
+  }
 }
